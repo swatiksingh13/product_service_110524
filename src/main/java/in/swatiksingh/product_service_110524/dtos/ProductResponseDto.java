db60b7939103489f97f1cb3f2a67643485703ebe
Swati Kumari Singh <swati.ksingh13@gmail.com>
2024-05-22 12:08:35 +0530

    Add self product service with own DB

--- src/main/java/in/swatiksingh/product_service_110524/dtos/ProductResponseDto.java
+++ src/main/java/in/swatiksingh/product_service_110524/dtos/ProductResponseDto.java
@@ -11,6 +11,6 @@ public class ProductResponseDto {
     private String title;
     private String description;
     private double price;
-    private String image;
+    private String imageUrl;
     private String category;
 }
